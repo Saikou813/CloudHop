5806629085c62692ceaf1c4dbe18b7e62ea656d3
Saikou813 <saiksuso@gmail.com>
2026-03-08 02:02:38 +0000

    Implement booking_list view and URL routing

--- flights/urls.py
+++ flights/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.FlightList.as_view(), name='home'),
     path('book/<int:flight_id>/', views.book_flight, name='book_flight'),
+    path('my-bookings/', views.my_bookings, name='my_bookings'),
 ]
