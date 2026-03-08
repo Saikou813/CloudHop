1204b47619e6b0f96954bf5d0d68459758bb28c3
Saikou813 <saiksuso@gmail.com>
2026-03-08 14:13:43 +0000

    Implement Cancel and Edit functionality

--- flights/urls.py
+++ flights/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('book/<int:flight_id>/', views.book_flight, name='book_flight'),
     path('my-bookings/', views.booking_list, name='my_bookings'),
     path('cancel-booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
+    path('edit-booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
 ]
