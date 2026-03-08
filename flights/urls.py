4315dec39d93d0c52e8550b5b933eb1f0c8a8518
Saikou813 <saiksuso@gmail.com>
2026-03-08 02:36:00 +0000

    Complete UI, navigation, and user alerts

--- flights/urls.py
+++ flights/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.FlightList.as_view(), name='home'),
     path('book/<int:flight_id>/', views.book_flight, name='book_flight'),
-    path('my-bookings/', views.my_bookings, name='my_bookings'),
+    path('my-bookings/', views.booking_list, name='my_bookings'),
+    path('cancel-booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
 ]
