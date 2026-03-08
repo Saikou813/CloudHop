2b15c46e5c2047c492ba49c2f05fa47c16e874e3
Saikou813 <saiksuso@gmail.com>
2026-03-08 15:41:39 +0000

    bootstrap cdn link fixed

--- flights/tests.py
+++ flights/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Flight
 
-# Create your tests here.
+class FlightModelTest(TestCase):
+    def setUp(self):
+        # Create a "dummy" flight for testing
+        self.flight = Flight.objects.create(
+            flight_number="CH777",
+            destination="London",
+            departure_time="2026-05-01 12:00:00+00:00",
+            price=150.00
+        )
+
+    def test_flight_creation(self):
+        """Test that the flight was created with the correct data"""
+        self.assertEqual(self.flight.destination, "London")
+        self.assertEqual(str(self.flight), "CH777 to London")
