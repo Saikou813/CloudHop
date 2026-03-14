96c0c36c6b3f5a3232d94127b4be416671a68008
Saikou813 <saiksuso@gmail.com>
2026-03-14 23:40:51 +0000

    Add Cloudinary images to flight cards and fix loop syntax

--- flights/models.py
+++ flights/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 # This is the "Model" part of the MVC (Model-View-Controller) pattern.
@@ -19,7 +20,8 @@ class Flight(models.Model):
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     departure_time = models.DateTimeField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-
+    flight_image = CloudinaryField('image', default='placeholder')
+    
     def __str__(self):
         return f"{self.flight_number}: {self.origin} to {self.destination}"
 
