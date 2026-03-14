392f0d94997ade8848de8bec25eefc385cd60135
Saikou813 <saiksuso@gmail.com>
2026-03-14 04:38:01 +0000

    Add profile model, view, and signals logic

--- flights/forms.py
+++ flights/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Booking
+from .models import Booking, PassengerProfile
 
 class BookingForm(forms.ModelForm):
     class Meta:
@@ -8,4 +8,13 @@ class BookingForm(forms.ModelForm):
         widgets = {
             'passenger_name': forms.TextInput(attrs={'class': 'form-control'}),
             'seat_preference': forms.Select(attrs={'class': 'form-select'}),
+        }
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = PassengerProfile
+        fields = ['passport_number', 'frequent_flyer_number']
+        widgets = {
+            'passport_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Passport Number'}),
+            'frequent_flyer_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter FF Number'}),
         }
